bc929e0d0a998bdd14c9fc2b551224e791f08da5
hardik867 <144901931+hardik867@users.noreply.github.com>
2025-01-20 15:26:35 +0530

    Delete  .py

--- Assignment 6.2 .py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the number:")) ; 
-def AddOdd(n):
-    sum=0
-    for i in range(1,n+1):
-        if(i%2!=0):
-         sum+=i
-    return sum ; 
-
-print(f"The sum of first {n} odd numbers {AddOdd(n)}") ; 
